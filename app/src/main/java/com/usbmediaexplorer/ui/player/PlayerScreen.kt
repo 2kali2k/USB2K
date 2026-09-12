797c91aedd593bdc7e8b219d42c892a3a9f07739
2kali2k <heshamalhassani778@gmail.com>
2026-09-12 22:07:53 +0000

    fix: remove stray player import brace

--- app/src/main/java/com/usbmediaexplorer/ui/player/PlayerScreen.kt
+++ app/src/main/java/com/usbmediaexplorer/ui/player/PlayerScreen.kt
@@ -2,7 +2,6 @@ package com.usbmediaexplorer.ui.player
 
 import android.app.Activity
 import android.content.Context
-            }
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.media.AudioManager
